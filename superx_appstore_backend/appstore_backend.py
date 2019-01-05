8021b137d76f2215a531cd23e01c2e97946c2807
Wrishiraj Kaushik <wrix@libresoft.in>
2019-01-06 02:54:37 +0530

    * Show relavent button in the main page * isInstalled fixed. * Refresh apt.Cache() when one app is installed/remove more than once.

--- superx_appstore_backend/appstore_backend.py
+++ superx_appstore_backend/appstore_backend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import apt
+from apt import Cache
 import gi
 gi.require_version("AppStream", "1.0")
 from gi.repository import AppStream
@@ -84,7 +84,8 @@ class AppStoreBackend():
             'id': app.props.id,
             'pkg': app.get_pkgnames(),
             'name': app.props.name,
-            'summery': app.props.summary
+            'summery': app.props.summary,
+            'isInstalled': self.isInstalled(app.get_pkgname())
         }
         icon = app.get_icon_by_size(128, 128)
         if icon == None:
@@ -143,14 +144,30 @@ class AppStoreBackend():
     # More functionality will be added to this function in the future.
     # TODO: Add current version number and available version number
     def isInstalled(self, pkg):
+        if len(self.transacted) != len(set(self.transacted)):
+            # Refresh cache when duplicates are found.
+            self.transacted = []
+            self.apt_cache = Cache()
         try:
-            return self.apt_cache[pkg].is_installed
+             installed = self.apt_cache[pkg].is_installed
         except KeyError:
             return False
 
+        if pkg in self.transacted and installed:
+            return False
+        elif pkg in self.transacted and not installed:
+            return True
+        else:
+            return installed
+
+
+    def reloadAptCache(self):
+        self.apt_cache = Cache()
+
     def __init__(self):
-        self.apt_cache = apt.Cache()
+        self.apt_cache = Cache()
         self.pool = AppStream.Pool()
         self.pool.load()
+        self.transacted = []
         
 
