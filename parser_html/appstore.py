e754fb7ce598396fac4d85e767902f1790a20dee
Deep Jyoti Choudhury <deep.choudhury@libresoft.in>
2019-01-08 12:51:04 +0530

    * AppDetails issue fixed * Color issue of appBox of index and search page fixed

--- parser_html/appstore.py
+++ parser_html/appstore.py
@@ -120,7 +120,9 @@ def detail():
     for app_id in app_id_list:
         if app_id != id:
             related_app_list.append(backend_obj.appSummery(app_id))
-
+    
+    print("related_app_list")
+    print(related_app_list)
     return render_template('appdetails.html',
                            data=datas, addons = addons,
                            description = new_string,
