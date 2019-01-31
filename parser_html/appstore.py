06169628137a5a503fe769a936cb98fadfde0f96
Deep Jyoti Choudhury <deep.choudhury@libresoft.in>
2019-01-31 20:50:44 +0530

    no icon found issue in task page fixed

--- parser_html/appstore.py
+++ parser_html/appstore.py
@@ -181,6 +181,9 @@ def search():
 
 @app.route('/tasks/')
 def task():
+    print('tasks')
+    print(task)
+    print("current_task_details")
     print(current_task_details)
     return render_template('task.html', tasks=tasks,
                            current_task=current_task_details)
